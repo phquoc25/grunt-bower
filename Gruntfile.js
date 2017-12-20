0e46802d7c36212ee5dfce8ac905ca7ab3dd9333
quoc phan <phquoc25@gmail.com>
2017-12-21 01:12:41 +0700

    Config concat task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,18 @@
 module.exports = function(grunt) {
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json')
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            options: {
+                // define a string to put between each file in the concatenated output
+                separator: ';'
+            },
+            dist: {
+                // the files to concatenate
+                src: ['src/**/*.js'],
+                // the location of the resulting JS file
+                dest: 'dist/<%= pkg.name%>.js'
+            }
+        }
     });
+
 };
