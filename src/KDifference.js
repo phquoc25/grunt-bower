883b3e06e7f139b786f715d1d2c2609cab705202
quoc phan <phquoc25@gmail.com>
2018-01-05 08:28:16 +0700

    Apply OOP

--- src/KDifference.js
+++ src/KDifference.js
@@ -1,16 +1,32 @@
-function kDifference(inputArr, k) {
-    var numberRead = {};
-    var counter = 0;
-    inputArr.forEach(function(number) {
-        var greaterNumberToFind = number + 2;
-        var lesserNumberToFind = number > 2 ? number - 2 : undefined;
-        if(numberRead[greaterNumberToFind]) {
-            counter++;
-        }
-        if(lesserNumberToFind !== undefined && numberRead[lesserNumberToFind]) {
-            counter++;
-        }
-        numberRead[number] = 1;
-    });
-    return counter;
+function KDifference(inputArr, k) {
+    var paires = [];
+    var compute = function (inputArr, k) {
+        var numberRead = {};
+        var paires = [];
+        inputArr.forEach(function(number) {
+            var greaterNumberToFind = number + 2;
+            var lesserNumberToFind = number > 2 ? number - 2 : undefined;
+            var paireFound;
+            if(numberRead[greaterNumberToFind]) {
+                paireFound = {};
+                paireFound[greaterNumberToFind] = number;
+                paires.push(paireFound);
+            }
+            if(lesserNumberToFind !== undefined && numberRead[lesserNumberToFind]) {
+                paireFound = {};
+                paireFound[lesserNumberToFind] = number;
+                paires.push(paireFound);
+            }
+            numberRead[number] = 1;
+        });
+        console.log(paires);
+        return paires;
+    };
+    paires = compute(inputArr, k);
+    this.getNumberOfPaires = function() {
+        return paires.length;
+    };
+    this.getPaires = function() {
+        return this.paires;
+    };
 }
